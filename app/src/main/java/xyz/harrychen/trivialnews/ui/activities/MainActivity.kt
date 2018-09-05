1bd85d70efe699a7d1dab05d84f78416a9a4f72f
Harry Chen <i@harrychen.xyz>
2018-09-05 20:46:29 +0800

    Add drawer for main activity
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- app/src/main/java/xyz/harrychen/trivialnews/ui/activities/MainActivity.kt
+++ app/src/main/java/xyz/harrychen/trivialnews/ui/activities/MainActivity.kt
@@ -3,7 +3,8 @@ package xyz.harrychen.trivialnews.ui.activities
 import android.arch.lifecycle.Lifecycle
 import android.os.Bundle
 import android.support.design.internal.BottomNavigationItemView
-import android.support.design.widget.BottomNavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import com.miguelcatalan.materialsearchview.MaterialSearchView
@@ -11,6 +12,7 @@ import com.borax12.materialdaterangepicker.date.DatePickerDialog
 import com.jakewharton.rxbinding2.view.clicks
 import com.trello.rxlifecycle2.android.lifecycle.kotlin.bindUntilEvent
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_content.*
 import org.jetbrains.anko.*
 import org.joda.time.LocalDate
 import xyz.harrychen.trivialnews.R
@@ -26,9 +28,48 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
     private var fragments: HashMap<Int, BaseTimelineFragment> = HashMap()
 
     private fun initNavigation() {
-        main_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        main_navigation.setOnNavigationItemSelectedListener {
+            if (it.itemId != nowFragmentId) {
+                var fragment = fragments[it.itemId]
+                if (fragment == null) {
+                    fragment = createFragment(it.itemId)
+                    fragment!!.setCoordinatorLayout(main_coordinator)
+                }
+                fragments[it.itemId] = fragment
+                supportFragmentManager.beginTransaction().replace(R.id.main_frame, fragment).commit()
+
+            } else {
+                fragments[it.itemId]!!.scrollAndRefresh()
+
+            }
+            nowFragmentId = it.itemId
+            true
+        }
+
         findViewById<BottomNavigationItemView>(R.id.navigation_timeline).callOnClick()
+
     }
+
+
+    private fun initDrawer() {
+
+        val toggle = ActionBarDrawerToggle(this,
+                main_drawer, main_toolbar, R.string.main_drawer_open, R.string.main_drawer_close)
+        main_drawer.addDrawerListener(toggle)
+        toggle.isDrawerIndicatorEnabled =true
+        toggle.syncState()
+
+        main_drawer_navigation.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.drawer_manage_subscription -> Unit
+                R.id.drawer_logout -> Unit
+            }
+            true
+        }
+
+    }
+
 
     private val createFragment = { id: Int ->
         when (id) {
@@ -48,34 +89,19 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
         }
     }
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        if (nowFragmentId == item.itemId) {
-            fragments[nowFragmentId]!!.scrollAndRefresh()
-        } else {
-            var fragment = fragments[item.itemId]
-            if (fragment == null) {
-                fragment = createFragment(item.itemId)
-                fragment!!.setCoordinatorLayout(main_coordinator)
-            }
-            fragments[item.itemId] = fragment
-            supportFragmentManager.beginTransaction().replace(R.id.main_frame, fragment).commit()
-        }
-
-        nowFragmentId = item.itemId
-        true
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setSupportActionBar(main_toolbar)
 
         initNavigation()
         initToolbarAction()
+        initDrawer()
         initFloatingActionBar()
 
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main_toolbar, menu)
@@ -86,12 +112,18 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
         return true
     }
 
+
     override fun onBackPressed() {
-        when (main_search.isSearchOpen) {
-            true -> main_search.closeSearch()
-            false -> super.onBackPressed()
+
+        when {
+            main_drawer.isDrawerOpen(GravityCompat.START) ->
+                main_drawer.closeDrawer(GravityCompat.START)
+            main_search.isSearchOpen -> main_search.closeSearch()
+            else -> super.onBackPressed()
         }
+
     }
+
 
     private val filterDateRange = { _: DatePickerDialog,
                                     year: Int, month: Int, day: Int,
@@ -112,6 +144,7 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
         Unit
     }
 
+
     private fun showDateChooserDialog() {
         val today = LocalDate.now()
         val dialog = DatePickerDialog.newInstance(DatePickerDialog.OnDateSetListener(filterDateRange),
@@ -121,6 +154,7 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
         @Suppress("DEPRECATION")
         dialog.show(fragmentManager, "DateRangePicker")
     }
+
 
     private fun initToolbarAction() {
         main_search.setOnQueryTextListener(object:MaterialSearchView.OnQueryTextListener{
@@ -133,8 +167,9 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
                 return false
             }
         })
-
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
+
 
     private fun initFloatingActionBar() {
         main_fab.clicks().bindUntilEvent(this, Lifecycle.Event.ON_DESTROY).subscribe{
